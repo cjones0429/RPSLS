98609a0b8ae509ad37765e225cc2227ed23e9ec5
cjones0429 <cejones1997@aim.com>
2021-05-20 09:15:54 -0400

    added multiplayer and rounds function

--- rungame.py
+++ rungame.py
@@ -27,3 +27,27 @@ class Rungame:
               "Paper disproves Spock\n"
               "Spock vaporizes Rock\n")
 
+    def multiplayer(self):
+        try:
+            user_input = int(input("Press '1' for SinglePlayer and '2' for MultiPlayer"))
+            if user_input == 1:
+                return False
+            elif user_input == 2:
+                return True
+        except:
+            print("Invalid Input. Please press the number key '1' for SinglePlayer or '2' for Multiplayer")
+            return self.multiplayer()
+
+    def rounds(self):
+        try:
+            number = int(input("How many rounds to win? Please enter a number greater than or equal to 2  (i.e 2,3,4)"))
+            if number < 2:
+                print("You need to win at least 2 rounds")
+                return self.rounds()
+            return number
+        except:
+            print("Please enter a number using the numeric keys")
+            return self.rounds()
+
+
+
