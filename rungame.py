e23d188e1a00c92ae634d940324c69802314ee4e
cjones0429 <cejones1997@aim.com>
2021-05-20 09:17:57 -0400

    added player picks action and computer action function

--- rungame.py
+++ rungame.py
@@ -27,6 +27,21 @@ class Rungame:
               "Paper disproves Spock\n"
               "Spock vaporizes Rock\n")
 
+    def start_game(self):
+        multiplayer = self.multiplayer()
+        rounds_to_win = self.rounds()
+        if multiplayer:
+            self.player_two = Human("Player 2")
+        while self.player_one.win_count < rounds_to_win and self.player_two.win_count < rounds_to_win:
+            self.player_picks_action(self.player_one)
+            if self.player_two.name == "Player 2":
+                self.player_picks_action(self.player_two)
+            else:
+                self.computer_action()
+            self.results()
+        self.display_winner()
+        self.play_again()
+
     def multiplayer(self):
         try:
             user_input = int(input("Press '1' for SinglePlayer and '2' for MultiPlayer"))
@@ -48,6 +63,29 @@ class Rungame:
         except:
             print("Please enter a number using the numeric keys")
             return self.rounds()
+
+    def player_picks_action(self, player_turn):
+        try:
+            print(f"{player_turn.name}: It's your turn!")
+            print("Please enter a number 1-5 using numeric keys")
+            user_input = int(input(f"Round {self.round}\n"
+                                   f"Score: {self.player_one.name} has {self.player_one.win_count}\n"
+                                   f"{self.player_two.name} has {self.player_two.win_count}\n"
+                                   f"1: Rock\n"
+                                   f"2: Paper\n"
+                                   f"3: Scissors\n"
+                                   f"4: Lizard\n"
+                                   f"5: Spock\n"
+                                   f"Your pick:")) - 1
+            player_turn.action = player_turn.hand_gestures[user_input]
+            assert user_input >= 0
+        except:
+            self.player_picks_action(player_turn)
+        print("\n" * 100)
+
+    def computer_action(self):
+        print("Wall-e is picking his gesture wisely...")
+        self.player_two.pick_gesture()
 
 
 
